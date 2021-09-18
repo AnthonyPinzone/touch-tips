f41eceb81a739a4e842f502f7d7a6703d40a42cb
Anthony Pinzone <apinzone@geekhive.com>
2021-09-18 12:59:37 -0400

    Corrected issue with record display. Added rank and record for week 2.

--- src/components/Scores/SeriesRecord.js
+++ src/components/Scores/SeriesRecord.js
@@ -2,9 +2,9 @@ import React from "react";
 import getSeriesRecord from "../../utilities/getSeriesRecord";
 
 const SeriesRecord = ({ team1, team2 }) => (
-  <div className="bg-cyan-500 bg-opacity-50 border border-cyan-500 text-white inline-flex items-center justify-center text-center rounded-sm self-start p-1 mb-1 md:py-0 md:px-2 xl:absolute xl:left-1/2 xl:transform xl:-translate-x-1/2 xl:-translate-y-10">
-    <span className="uppercase text-sm mr-3 md:text-base">Series Record</span>
-    <span className="text-xl leading-none md:text-2xl">
+  <div className="bg-cyan-500 bg-opacity-50 border border-cyan-500 text-white flex items-center justify-center rounded-sm align-middle px-3 py-1 mb-1 w-40 md:py-0 md:px-2 xl:absolute xl:left-1/2 xl:transform xl:-translate-x-1/2 xl:-translate-y-10">
+    <span className="uppercase text-sm md:text-base">Series Record</span>
+    <span className="flex-1 text-center text-xl leading-none md:text-2xl">
       {getSeriesRecord(team1, team2)}
     </span>
   </div>
